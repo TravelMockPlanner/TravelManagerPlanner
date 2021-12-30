cdcfe0eda3e32b981231970058e8edce02543881
UyeonS <dusdl14@gmail.com>
2021-12-31 08:30:52 +0900

    # <타입>: <제목> DetailView UI 구성 ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    CollectionView를 이용한 Image paging과 태그에 관한 Label은 스크롤뷰를 이용하여 추가 예정
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- TravelManagerPlanner/TravelManagerPlanner/ViewController/DetailViewController.swift
+++ TravelManagerPlanner/TravelManagerPlanner/ViewController/DetailViewController.swift
@@ -10,16 +10,102 @@ import SnapKit
 
 class DetailViewController: UIViewController {
     
-    lazy var imageView: UIImageView = {
-        var imageView = UIImageView()
-        return imageView
+    let images: [UIImage] = [UIImage(named: "Seoul1")!, UIImage(named: "Seoul2")!, UIImage(named: "Seoul3")!, UIImage(named: "Seoul4")!]
+    
+    lazy var detailScrollView: UIScrollView = {
+        var scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    lazy var imageAnchorView: UIView = {
+        var view = UIView()
+        return view
+    }()
+    
+    lazy var imageSliderCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ImageSliderCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    lazy var imagePageControl: UIPageControl = {
+        var pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        return pageControl
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Sample"
+        return label
+    }()
+    lazy var descTextView: UITextView = {
+        var textView = UITextView()
+        textView.text = "Sample"
+        return textView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
+        setImageColletionView()
         // Do any additional setup after loading the view.
     }
-
-
+    func setLayout() {
+        view.addSubview(imageAnchorView)
+        imageAnchorView.addSubview(imageSliderCollectionView)
+        imageAnchorView.addSubview(imagePageControl)
+        
+        imageAnchorView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.snp.centerY)
+            make.width.equalToSuperview()
+        }
+        imageSliderCollectionView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(imageAnchorView.snp.width).multipliedBy(0.8)
+            make.height.equalTo(imageSliderCollectionView.snp.width)
+        }
+        imagePageControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-30)
+            make.width.equalToSuperview().multipliedBy(0.7)
+            
+        }
+    }
+    func setImageColletionView(){
+        imageSliderCollectionView.delegate = self
+        imageSliderCollectionView.dataSource = self
+    }
+}
+extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        imagePageControl.numberOfPages = images.count
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imageSliderCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageSliderCollectionViewCell
+        let image = images[indexPath.row]
+        cell.setCollectionViewIngredient(image: image)
+        return cell
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if let indexPath = imageSliderCollectionView.indexPathsForVisibleItems.first {
+                imagePageControl.currentPage = indexPath.row
+            }
+    }
+}
+extension DetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return imageSliderCollectionView.frame.size
+    }
+    
 }
 
